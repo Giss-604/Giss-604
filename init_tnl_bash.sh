fd24a861b1c5d96b34e917a5cf1ee76916c9f565
tnl <ntausnev@gmail.com>
2024-01-23 18:10:36 -0500

    Added gitignore and git config links

--- init_tnl_bash.sh
+++ init_tnl_bash.sh
@@ -4,9 +4,14 @@ if [ -r "${HOME}/.bashrc" ]; then
   rm -rf ${HOME}/.bashrc
 fi
 rm -rf ${HOME}/BASH_HOME
+rm ~/.gitconfig
+rm ~/.gitignore
 
 git clone git@github.com:Giss-604/tnl.git ~/BASH_HOME_4git 
 if [ $? -eq 0 ]; then
   ln -sf ${HOME}/BASH_HOME_4git/BASH_HOME ~/BASH_HOME
   ln -sf ${HOME}/BASH_HOME/DOT_DIR/bashrc ~/.bashrc
+  ln -sf ${HOME}/BASH_HOME/DOT_DIR/gitconfig ~/.gitconfig
+  ln -sf ${HOME}/BASH_HOME/DOT_DIR/gitignore ~/.gitignore
+
 fi
